54e63951afd183c644a10e73a190bd2401dcf32c
mdiz <mike.dismore@voyantsolutions.com>
2020-05-19 14:47:44 -0400

    Update

--- Adeept Starter Kit/Adeept_Ultimate_Starter_Kit_for_RPi_V1.1/code/Adeept_Ultimate_Starter_Kit_Python_Code_for_RPi-master/01_blinkingLed_1.py
+++ Adeept Starter Kit/Adeept_Ultimate_Starter_Kit_for_RPi_V1.1/code/Adeept_Ultimate_Starter_Kit_Python_Code_for_RPi-master/01_blinkingLed_1.py
@@ -9,24 +9,32 @@
 # Date        : 2015/06/12
 #-----------------------------------------------------------
 
+#5/19/20
+# Lesson Completed
+# Code tabing and print command corrected
+
+#!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
-
-LedPin = 11    # pin11
-
-GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
-GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
-GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the led
-
-try:
+LedPin = 11 # pin11
+def setup():
+	GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT) # Set LedPin's mode is output
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+def loop():
 	while True:
-		print '...led on'
-		GPIO.output(LedPin, GPIO.LOW)  # led on
+		print('...led on')
+		GPIO.output(LedPin, GPIO.LOW) # led on
 		time.sleep(0.5)
-		print 'led off...'
+		print('led off...')
 		GPIO.output(LedPin, GPIO.HIGH) # led off
 		time.sleep(0.5)
-except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be  executed.
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
-
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH) # led off
+	GPIO.cleanup() # Release resource
+if __name__ == '__main__': # Program start from here
+	setup()
+try:
+	loop()
+except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
+	destroy()
